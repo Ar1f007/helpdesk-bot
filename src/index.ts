22396904c29986a09561211e0df16151de6f227d
Ar1f007 <muhammadarif9416@yahoo.com>
2022-08-05 20:26:30 +0600

    add rest api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-import './bot';
+import { client } from './bot';
+import { createRestApi } from './rest-api';
+
+const PORT = process.env.PORT || 5000;
+
+const api = createRestApi(client);
+
+api.listen(PORT, () => console.log(`[rest-api]: Bot rest-api is running at ${PORT}`));
